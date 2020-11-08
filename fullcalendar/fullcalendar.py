36aa8462e8b0bf78aaca3b489e8ddd6c83691982
Michael DM Dryden <mk.dryden@utoronto.ca>
2020-11-08 02:05:10 -0500

    Simplify tags

--- fullcalendar/fullcalendar.py
+++ fullcalendar/fullcalendar.py
@@ -1,33 +1,49 @@
 from django.conf import settings
+from django.templatetags.static import static
 
 # django-fullcalendar static file location defaults to FullCalendar default 
 # folder structure, expected to be under the STATIC_URL
 
-FULLCALENDAR_DEFAULTS = {
-    'css_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.css',
-    'print_css_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.print.css',
-    'javascript_url': '//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.min.js',
-    'jquery_url': '//code.jquery.com/jquery-2.1.0.min.js',
-    'jquery_ui_url': '//code.jquery.com/ui/1.10.4/jquery-ui.js',
+FULLCALENDAR_DEFAULTS: dict[str] = {
+    'fullcalendar_css_url': static('fullcalendar/main.min.css'),
+    'fullcalendar_url': static('fullcalendar/main.min.js'),
+    'jquery_url': static('jquery/dist/jquery.js'),
+    'jquery_ui_url': static('jquery-ui-dist/jquery-ui.js'),
+    'bootstrap_css_url': static('bootstrap/dist/css/bootstrap.min.css'),
+    'fontawesome_css_url': 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.css',
 }
 
 # Updates location based on configuration defined by 
 # settings.py of the project
 
 FULLCALENDAR = FULLCALENDAR_DEFAULTS.copy()
-FULLCALENDAR.update(getattr(settings, 'FULLCALENDAR', {}))
+FULLCALENDAR |= getattr(settings, 'FULLCALENDAR', {})
+
+
+def get_url(key: str) -> str:
+    return FULLCALENDAR[key]
+
 
 def css_url():
     return FULLCALENDAR['css_url']
 
-def print_css_url():
-    return FULLCALENDAR['print_css_url']
 
 def javascript_url():
     return FULLCALENDAR['javascript_url']
 
+
 def jquery_url():
     return FULLCALENDAR['jquery_url']
 
+
 def jquery_ui_url():
     return FULLCALENDAR['jquery_ui_url']
+
+
+def bootstrap_css_url():
+    return FULLCALENDAR['bootstrap_css_url']
+
+
+def fontawesome_css_url():
+    return FULLCALENDAR['bootstrap_css_url']
+
--- fullcalendar/templatetags/fullcalendar_tags.py
+++ fullcalendar/templatetags/fullcalendar_tags.py
@@ -1,59 +1,26 @@
 from django import template
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
-from ..fullcalendar import css_url, print_css_url, javascript_url, jquery_url, jquery_ui_url
+from ..fullcalendar import get_url
 
 register = template.Library()
 
 
-@register.inclusion_tag("fullcalendar/calendar.html")
-def calendar():
-    return {}
+@register.simple_tag
+def calendar(calendar_id: str = 'calendar'):
+    return format_html("<div id='{}'></div>", calendar_id)
 
-@register.inclusion_tag("fullcalendar/calendar_init.html")
-def calendar_init(calendar_config_options):
-    return {'calendar_config_options': mark_safe(calendar_config_options)}
+
+@register.inclusion_tag('fullcalendar/calendar_init.html')
+def calendar_init(calendar_id: str = 'calendar', calendar_config_options: str = None):
+    return {'calendar_id': mark_safe(calendar_id), 'calendar_config_options': mark_safe(calendar_config_options)}
+
 
 @register.simple_tag
-def fullcalendar_css_url():
-    return css_url()
+def fullcalendar_css(url: str) -> str:
+    return format_html("<link href='{}' rel='stylesheet' />", get_url(url))
+
 
 @register.simple_tag
-def fullcalendar_print_css_url():
-    return print_css_url()
-
-@register.simple_tag
-def fullcalendar_javascript_url():
-    return javascript_url()
-
-@register.simple_tag
-def fullcalendar_jquery_url():
-    return jquery_url()
-
-@register.simple_tag
-def fullcalendar_jquery_ui_url():
-    return jquery_ui_url()
-
-@register.simple_tag
-def fullcalendar_css():
-    url = fullcalendar_css_url()
-    return mark_safe("<link href='%s' rel='stylesheet' />" % url)
-
-@register.simple_tag
-def fullcalendar_print_css():
-    url = fullcalendar_print_css_url()
-    return mark_safe("<link href='%s' rel='stylesheet' media='print' />" % url)
-
-@register.simple_tag
-def fullcalendar_jquery():
-    url = fullcalendar_jquery_url()
-    return mark_safe("<script src='%s'></script>" % url)
-
-@register.simple_tag
-def fullcalendar_jquery_ui():
-    url = fullcalendar_jquery_ui_url()
-    return mark_safe("<script src='%s'></script>" % url)
-
-@register.simple_tag
-def fullcalendar_javascript():
-    url = fullcalendar_javascript_url()
-    return mark_safe("<script src='%s'></script>" % url)
+def fullcalendar_js(url: str) -> str:
+    return format_html("<script src='{}'></script>", get_url(url))
